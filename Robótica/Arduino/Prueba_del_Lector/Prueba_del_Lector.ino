4b6c6d4b685995c0ab421ae5453c9df29284b735
Juacomaster <joa.dan.cal@gmail.com>
2024-07-21 23:32:43 +0200

    Update Prueba_del_Lector.ino

--- Robótica/Arduino/Prueba_del_Lector/Prueba_del_Lector.ino
+++ Robótica/Arduino/Prueba_del_Lector/Prueba_del_Lector.ino
@@ -1,16 +1,36 @@
-int Lector=12;
+int Lector=5;
+int Lector2=6;
+int Lector3=7;
+int Lector4=8;
+
+
 bool Luz=0;
+bool Luz2=0;
+bool Luz3=0;
+bool Luz4=0;
 
 void setup() {
 Serial.begin(9600);
 pinMode(Lector,INPUT);
+pinMode(Lector2,INPUT);
+pinMode(Lector3,INPUT);
+pinMode(Lector4,INPUT);
 
 }
 
 void loop() {
 
 Luz=digitalRead(Lector);
-Serial.println(Luz);
+Luz2=digitalRead(Lector2);
+Luz3=digitalRead(Lector3);
+Luz4=digitalRead(Lector4);
+Serial.print(Luz);
+Serial.print("      ");
+Serial.print(Luz2);
+Serial.print("      ");
+Serial.print(Luz3);
+Serial.print("      ");
+Serial.println(Luz4);
 delay(100);
 
 }
